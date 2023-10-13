8d9fee3d4f69a84a497e2a87eac1045cb34e452b
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-13 14:52:20 +0300

    create Order page

---
+++ src/pages/Order/Order.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Layout from "../../components/Layout/Layout";
+
+function Order() {
+    return (
+        <Layout>
+            <h1>Order page</h1>
+        </Layout>
+    );
+}
+
+export default Order;
