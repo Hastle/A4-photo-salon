7a9ad0f0da58ab77a07ed14af1803911592c1556
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-13 14:51:08 +0300

    create Contacts page

---
+++ src/pages/Contacts/Contacts.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Layout from "../../components/Layout/Layout";
+
+function Contacts() {
+    return (
+        <Layout>
+            <h1>Contacts page</h1>
+        </Layout>
+    );
+}
+
+export default Contacts;
