5d3ff47b3c28a54226918e8c3498f6913509f42c
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-13 14:57:45 +0300

    remove comments CategoryInfo component

--- src/components/CategoryInfo/CategoryInfo.js
+++ src/components/CategoryInfo/CategoryInfo.js
@@ -2,18 +2,15 @@ import React from "react";
 import photoServicesList from "../../data/photoServicesList";
 
 const CategoryInfo = ({ officeId, categoryName }) => {
-    // Найдем данные для указанного officeId
+
     const officeData = photoServicesList.find((item) => item.id === officeId);
 
-    // Проверим, что данные для этого officeId были найдены
     if (!officeData) {
         return <p>Данные для officeId {officeId} не найдены.</p>;
     }
 
-    // Далее, попробуем найти информацию о категории
     const categoryData = officeData.services[categoryName];
 
-    // Проверим, что данные для этой категории были найдены
     if (!categoryData) {
         return <p>Данные для категории {categoryName} не найдены.</p>;
     }
@@ -25,14 +22,12 @@ const CategoryInfo = ({ officeId, categoryName }) => {
                 <div key={key}>
                     <p>{key}</p>
                     {typeof categoryData[key] === 'object' ? (
-                        // Если значение - объект, обходим его и выводим ключи и значения
                         Object.keys(categoryData[key]).map((innerKey) => (
                             <p key={innerKey}>
                                 {innerKey} {categoryData[key][innerKey]}
                             </p>
                         ))
                     ) : (
-                        // Иначе выводим значение
                         <p>{categoryData[key]}</p>
                     )}
                 </div>
