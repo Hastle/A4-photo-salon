687e9eab79aeba079fc9f23dcdb89f0b3ce78795
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-13 14:52:36 +0300

    create PhotoServices page

---
+++ src/pages/PhotoServices/PhotoServices.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Layout from "../../components/Layout/Layout";
+import CategoryInfo from "../../components/CategoryInfo/CategoryInfo";
+
+function PhotoServices() {
+    return (
+        <Layout>
+            <div className="col-md-12">
+                <h3>Фотоуслуги</h3>
+            </div>
+            <CategoryInfo officeId={1} categoryName={'Фотопечать на глянцевой фотобумаге'} />
+        </Layout>
+    );
+}
+
+export default PhotoServices;
