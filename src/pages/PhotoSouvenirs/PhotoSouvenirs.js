42a0fea2b7c8f2c1091e30aa24ced4c176e5e028
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-13 14:52:55 +0300

    create PhotoSouvenirs page

---
+++ src/pages/PhotoSouvenirs/PhotoSouvenirs.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Layout from "../../components/Layout/Layout";
+
+function PhotoSouvenirs() {
+    return (
+        <Layout>
+            <h1>Photo Souvenirs page</h1>
+        </Layout>
+    );
+}
+
+export default PhotoSouvenirs;
