9210a82634cec446cf80eb2c74954bcb6dbc30db
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-13 14:52:10 +0300

    create OfficeServices page

---
+++ src/pages/OfficeServices/OfficeServices.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Layout from "../../components/Layout/Layout";
+
+function OfficeServices() {
+    return (
+        <Layout>
+            <h1>Office Services page</h1>
+        </Layout>
+    );
+}
+
+export default OfficeServices;
