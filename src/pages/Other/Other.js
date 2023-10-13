359738c5e66d7029d4b2f903abbc73fa09838766
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-13 14:52:28 +0300

    create Other page

---
+++ src/pages/Other/Other.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Layout from "../../components/Layout/Layout";
+
+function Other() {
+    return (
+        <Layout>
+            <h1>Other page</h1>
+        </Layout>
+    );
+}
+
+export default Other;
