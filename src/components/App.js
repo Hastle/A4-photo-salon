fcda2b297ffd5045c8798e3db113c4850a5ac587
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-15 07:57:37 +0300

    props selectedOfficeId for App

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "../pages/Main/Main";
 import PhotoServices from "../pages/PhotoServices/PhotoServices";
@@ -7,18 +7,28 @@ import OfficeServices from "../pages/OfficeServices/OfficeServices";
 import Other from "../pages/Other/Other";
 import Order from "../pages/Order/Order";
 import Contacts from "../pages/Contacts/Contacts";
+import Layout from "../components/Layout/Layout";
 
 import ScrollToTop from "../utils/ScrollToTop";
 
-
 function App() {
+
+	const [selectedOfficeId, setSelectedOfficeId] = useState(1);
+	const handleOfficeChange = (newOfficeId) => {
+		setSelectedOfficeId(newOfficeId);
+	};
+
 	return (
 
 			<BrowserRouter>
 				<ScrollToTop />
 				<Routes>
-					<Route path="/" exact="true" element={<Main />}/>
-					<Route path="/photo-services" element={<PhotoServices />}/>
+					<Route path="/" element={<Main />}/>
+					<Route path="/photo-services" element={
+						<Layout selectedOfficeId={selectedOfficeId} onOfficeChange={handleOfficeChange}>
+							<PhotoServices selectedOfficeId={selectedOfficeId} />
+						</Layout>
+					} />
 					<Route path="/photo-souvenirs" element={<PhotoSouvenirs />}/>
 					<Route path="/office-sevices" element={<OfficeServices />}/>
 					<Route path="/other" element={<Other />}/>
