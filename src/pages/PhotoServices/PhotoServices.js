ca373135c360c24d969920a64846205889464973
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-15 07:52:04 +0300

    props selectedOfficeId for PhotoServices page

--- src/pages/PhotoServices/PhotoServices.js
+++ src/pages/PhotoServices/PhotoServices.js
@@ -2,14 +2,17 @@ import React, { Component } from "react";
 import Layout from "../../components/Layout/Layout";
 import CategoryInfo from "../../components/CategoryInfo/CategoryInfo";
 
-function PhotoServices() {
+function PhotoServices(props) {
+
+    console.log(props.selectedOfficeId);
+
     return (
-        <Layout>
+        <div>
             <div className="col-md-12">
                 <h3>Фотоуслуги</h3>
             </div>
             <CategoryInfo officeId={1} categoryName={'Фотопечать на глянцевой фотобумаге'} />
-        </Layout>
+        </div>
     );
 }
 
