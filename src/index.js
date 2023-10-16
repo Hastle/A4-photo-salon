13b4a3dafdb883537c074bddad4a6ec7e36a282e
Hastle <alexandrs.me.com@gmail.com>
2023-10-17 02:44:10 +0300

    use context provider for useSelectedValue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app';
-import './styles/main.sass';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app";
+import { SelectedValueProvider } from "./context/SelectedValueContext";
+import "./styles/main.sass";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+
+    return (
+        <SelectedValueProvider>
+            <App />
+        </SelectedValueProvider>
+    );
+};
+
+ReactDOM.render(<Root />, document.getElementById('root'));
