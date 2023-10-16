e8b47f8f13a84110ba42ac18ddd03d25d82582c9
Hastle <alexandrs.me.com@gmail.com>
2023-10-17 02:45:06 +0300

    remove props drilling

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,25 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import styles from "./style.module.sass";
 
 function Layout(props) {
 
-    const [selectedOfficeId, setSelectedOfficeId] = useState(1);
-    const handleOfficeChange = (officeId) => {
-        setSelectedOfficeId(officeId);
-        props.onOfficeChange(officeId);
-    }
-
     return (
         <div>
-            <Header selectedOfficeId={selectedOfficeId} onOfficeChange={handleOfficeChange} />
+            <Header/>
             <div className={`container ${styles.wrapper}`}>
                 <div className="row">
                     {props.children}
                 </div>
             </div>
-            <Footer selectedOfficeId={selectedOfficeId} />
+            <Footer/>
         </div>
     );
 }
