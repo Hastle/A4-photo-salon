7305663541b520d9075055f85e08ac242b056578
Hastle <alexandrs.me.com@gmail.com>
2023-10-17 02:34:52 +0300

    remove props drilling

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "../pages/Main/Main";
 import PhotoServices from "../pages/PhotoServices/PhotoServices";
@@ -7,37 +7,24 @@ import OfficeServices from "../pages/OfficeServices/OfficeServices";
 import Other from "../pages/Other/Other";
 import Order from "../pages/Order/Order";
 import Contacts from "../pages/Contacts/Contacts";
-import Layout from "../components/Layout/Layout";
 
 import ScrollToTop from "../utils/ScrollToTop";
 
 function App() {
-
-	const [selectedOfficeId, setSelectedOfficeId] = useState(1);
-	const handleOfficeChange = (newOfficeId) => {
-		setSelectedOfficeId(newOfficeId);
-	};
-
 	return (
-
 			<BrowserRouter>
 				<ScrollToTop />
 				<Routes>
-					<Route path="/" element={<Main />}/>
-					<Route path="/photo-services" element={
-						<Layout selectedOfficeId={selectedOfficeId} onOfficeChange={handleOfficeChange}>
-							<PhotoServices selectedOfficeId={selectedOfficeId} />
-						</Layout>
-					} />
-					<Route path="/photo-souvenirs" element={<PhotoSouvenirs />}/>
-					<Route path="/office-sevices" element={<OfficeServices />}/>
-					<Route path="/other" element={<Other />}/>
-					<Route path="/order" element={<Order />}/>
-					<Route path="/contacts" element={<Contacts />}/>
+					<Route path="/" element={<Main />} />
+					<Route path="/photo-services" element={<PhotoServices />} />
+					<Route path="/photo-souvenirs" element={<PhotoSouvenirs />} />
+					<Route path="/office-services" element={<OfficeServices />} />
+					<Route path="/other" element={<Other />} />
+					<Route path="/order" element={<Order />} />
+					<Route path="/contacts" element={<Contacts />} />
 				</Routes>
 			</BrowserRouter>
-
-	)
+	);
 }
 
 export default App;
