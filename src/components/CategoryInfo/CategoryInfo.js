2c223ecd22922ff7a1ef0012dfe83079abf617b3
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-19 07:36:52 +0300

    change return with new structure of photoServicesList

--- src/components/CategoryInfo/CategoryInfo.js
+++ src/components/CategoryInfo/CategoryInfo.js
@@ -6,30 +6,29 @@ const CategoryInfo = ({ officeId, categoryName }) => {
     const officeData = photoServicesList.find((item) => item.id === officeId);
 
     if (!officeData) {
-        return <p>Данные для officeId {officeId} не найдены.</p>;
+        return <p>Данные для магазина {officeId} не найдены.</p>;
     }
 
-    const categoryData = officeData.services[categoryName];
+    const categoryData = officeData.services.find((service) => service.name === categoryName);
 
     if (!categoryData) {
         return <p>Данные для категории {categoryName} не найдены.</p>;
     }
 
+    if (!categoryData.products) {
+        return <p>Данные для продуктов категории {categoryName} не найдены.</p>;
+    }
+
     return (
         <div className="col-md-8">
             <h4>{categoryName}</h4>
-            {Object.keys(categoryData).map((key) => (
-                <div key={key}>
-                    <p>{key}</p>
-                    {typeof categoryData[key] === 'object' ? (
-                        Object.keys(categoryData[key]).map((innerKey) => (
-                            <p key={innerKey}>
-                                {innerKey} {categoryData[key][innerKey]}
-                            </p>
-                        ))
-                    ) : (
-                        <p>{categoryData[key]}</p>
-                    )}
+            {categoryData.products.map((product) => (
+                <div key={product.size}>
+                    <p>{product.size}</p>
+                    {product.options &&
+                        Object.keys(product.options).map((optionName) => (
+                            <p key={optionName}>{optionName} {product.options[optionName]}</p>
+                        ))}
                 </div>
             ))}
         </div>
