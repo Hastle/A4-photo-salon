79fe70cec07c3473bbe0f1d4625df5a5dafd40db
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-19 07:30:44 +0300

    use value from useSelectedValue

--- src/pages/PhotoServices/PhotoServices.js
+++ src/pages/PhotoServices/PhotoServices.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Layout from "../../components/Layout/Layout";
 import CategoryInfo from "../../components/CategoryInfo/CategoryInfo";
+import { useSelectedValue } from "../../context/SelectedValueContext";
+import officeList from "../../data/officeList";
+import photoServicesList from "../../data/photoServicesList";
 
 function PhotoServices() {
+    const { selectedValue } = useSelectedValue();
+    const [selectedOffice, setSelectedOffice] = useState(1);
+    const [selectedOfficeData, setSelectedOfficeData] = useState(null);
+
+    useEffect(() => {
+        const selectedOffice = officeList.find(office => office.id === selectedValue).id;
+        setSelectedOffice(selectedOffice);
+        const officeData = photoServicesList.find((office) => office.id === selectedValue);
+        setSelectedOfficeData(officeData);
+    }, [selectedValue]);
 
     return (
         <Layout>
             <div className="col-md-12">
                 <h3>Фотоуслуги</h3>
             </div>
-            <CategoryInfo officeId={1} categoryName={'Фотопечать на глянцевой фотобумаге'} />
+            {selectedOfficeData &&
+                selectedOfficeData.services.map((service) => (
+                    <CategoryInfo key={service.id} officeId={selectedOffice} categoryName={service.name} />
+                ))
+            }
         </Layout>
     );
 }
