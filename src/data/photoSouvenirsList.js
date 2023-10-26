81c70bd18b0f10d07a8e347249091c2a0fb97f1e
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-26 10:34:17 +0300

    create data photoServicesList

---
+++ src/data/photoSouvenirsList.js
@@ -0,0 +1,253 @@
+import Polaroid_1 from "../assets/images/photo-souvenirs/polaroid-1.png";
+import Polaroid_2 from "../assets/images/photo-souvenirs/polaroid-2.png";
+import Polaroid_5 from "../assets/images/photo-souvenirs/polaroid-5.png";
+import Polaroid_6 from "../assets/images/photo-souvenirs/polaroid-6.png";
+
+import Polaroid_3 from "../assets/images/photo-services/polaroid-3.png";
+import Polaroid_4 from "../assets/images/photo-services/polaroid-4.png";
+import CommonCard from "../assets/images/photo-services/common-card.png";
+
+const photoSouvenirsList = [
+    {
+        id: 1,
+        services: [
+            {
+                name: 'Кружки',
+                products: [
+                    {
+                        options: [
+                            {
+                                subtitle: 'Кружка белая снаружи',
+                                price: '700р'
+                            },
+                            {
+                                subtitle: 'Кружка-хамелеон',
+                                price: '950р'
+                            },
+                            {
+                                subtitle: 'Кружка с ложкой',
+                                price: '820р'
+                            },
+                            {
+                                subtitle: 'Кружка матовая/стеклянная',
+                                price: '550р'
+                            },
+                            {
+                                subtitle: 'Бутылка металлическая',
+                                price: '1300р'
+                            },
+                            {
+                                subtitle: 'Кружка пивная',
+                                price: '1200р'
+                            },
+                            {
+                                subtitle: 'Термокружка',
+                                price: '1200р'
+                            },
+                            {
+                                subtitle: 'Термос металлический',
+                                price: '1790р'
+                            },
+                        ],
+                        description: 'Доступны кружки с внутренней заливкой (жёлтые, оранжевые, красные, бордовые, черные, синие, голубые, мятные, розовые, лавандовые, бежевые, салатовые, бирюзовые). Наличие уточняйте у сотрудников'
+                    },
+                ],
+                sideImages: [Polaroid_1],
+            },
+            {
+                name: 'Футболки и толстовки',
+                products: [
+                    {
+                        options: [
+                            {
+                                subtitle: 'Футболка белая (с одной стороны)',
+                                price: 'от 1500р'
+                            },
+                            {
+                                subtitle: 'Печать на черной/цветной футболке',
+                                price: 'от 800р'
+                            },
+                            {
+                                subtitle: 'Печать на своей белой футболке',
+                                price: '600р'
+                            },
+                            {
+                                subtitle: 'Дополнительный отпечаток',
+                                price: '200р'
+                            },
+                            {
+                                subtitle: 'Толстовка',
+                                price: '2200р'
+                            },
+                        ]
+                    },
+                ],
+                sideImages: [Polaroid_2],
+            },
+            {
+                name: 'Пазлы',
+                products: [
+                    {
+                        options: [
+                            {
+                                subtitle: 'Пазл 13х18',
+                                price: '470р'
+                            },
+                            {
+                                subtitle: 'Пазл А4',
+                                price: '750р'
+                            },
+                            {
+                                subtitle: 'Пазл А3',
+                                price: '1110р'
+                            },
+                            {
+                                subtitle: 'Пазл сердце',
+                                price: '700р'
+                            },
+                            {
+                                subtitle: 'Магнитный пазл 13х18',
+                                price: '570р'
+                            },
+                            {
+                                subtitle: 'Манитный пазл А4',
+                                price: '850р'
+                            },
+                            {
+                                subtitle: 'Магнитный пазл сердце',
+                                price: '750р'
+                            },
+                            {
+                                subtitle: 'Пазл золото/серебро А4',
+                                price: '700р'
+                            },
+                            {
+                                subtitle: 'Пазл золото/серебро А3',
+                                price: '1000р'
+                            },
+                            {
+                                subtitle: 'Пазл золото/серебро сердце',
+                                price: '650р'
+                            },
+                        ]
+                    },
+                ]
+            },
+            {
+                name: 'Подушки',
+                products: [
+                    {
+                        options: [
+                            {
+                                subtitle: 'Детская подушка',
+                                price: '700р'
+                            },
+                            {
+                                subtitle: 'Плюшевая подушка',
+                                price: '1100р'
+                            },
+                            {
+                                subtitle: 'Атласная подушка',
+                                price: '1200р'
+                            },
+                            {
+                                subtitle: 'Подушка с кантом',
+                                price: '1400р'
+                            },
+                            {
+                                subtitle: 'Детская с узором',
+                                price: '1400р'
+                            },
+                            {
+                                subtitle: 'Льняная подушка',
+                                price: '1400р'
+                            },
+                            {
+                                subtitle: 'Подушка с сердцем',
+                                price: '1500р'
+                            },
+                            {
+                                subtitle: 'Подушка с бахромой',
+                                price: '1600р'
+                            },
+                            {
+                                subtitle: 'Подушка с рюшами',
+                                price: '1600р'
+                            },
+                            {
+                                subtitle: 'Подушка с пайетками',
+                                price: '1700р'
+                            },
+                        ]
+                    },
+                ]
+            },
+            {
+                products: [
+                    {
+                        options: [
+                            {
+                                subtitle: 'Шоппер',
+                                price: 'от 950р',
+                            },
+                            {
+                                subtitle: 'Магнит акриловый',
+                                price: 'от 170р',
+                            },
+                            {
+                                subtitle: 'Магнит виниловый',
+                                price: 'от 200р',
+                            },
+                            {
+                                subtitle: 'Брелок акриловый',
+                                price: '190р',
+                            },
+                            {
+                                subtitle: 'Брелок металлический',
+                                price: 'от 400р',
+                            },
+                            {
+                                subtitle: 'Брелок с кабелем для зарядки',
+                                price: '870р',
+                            },
+                            {
+                                subtitle: 'Коврик для мышки',
+                                price: '850р',
+                                sideImages: [Polaroid_5],
+                            },
+                            {
+                                subtitle: 'Обложка на автодокументы',
+                                price: '1200р',
+                            },
+                            {
+                                subtitle: 'Чехол на iPhone',
+                                price: 'от 150р',
+                            },
+                            {
+                                subtitle: 'Фотокамень',
+                                price: '950р',
+                            },
+                            {
+                                subtitle: 'Брелок гос. номер',
+                                price: '400р',
+                            },
+                            {
+                                subtitle: 'Ручка',
+                                price: '120р',
+                            },
+                            {
+                                subtitle: 'Линейка',
+                                price: '270р',
+                            },
+                        ]
+                    }
+                ],
+                sideImages: [Polaroid_5, Polaroid_6],
+            }
+            // Другие товары и услуги
+        ],
+    },
+    // Другие объекты для других id могут быть добавлены здесь, если необходимо.
+];
+
+export default photoSouvenirsList;
