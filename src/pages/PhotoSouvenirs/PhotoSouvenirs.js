483e8fc4e821cff5282df2968bbc9a1e3bc6d57f
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-26 10:49:40 +0300

    add useSelectedValue, CategoryInfo component

--- src/pages/PhotoSouvenirs/PhotoSouvenirs.js
+++ src/pages/PhotoSouvenirs/PhotoSouvenirs.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Layout from "../../components/Layout/Layout";
+import {useSelectedValue} from "../../context/SelectedValueContext";
+import officeList from "../../data/officeList";
+import photoSouvenirsList from "../../data/photoSouvenirsList";
+import CategoryInfo from "../../components/CategoryInfo/CategoryInfo";
 
 function PhotoSouvenirs() {
+    const { selectedValue } = useSelectedValue();
+    const [selectedOffice, setSelectedOffice] = useState(1);
+    const [selectedOfficeData, setSelectedOfficeData] = useState(null);
+
+    useEffect(() => {
+        const selectedOffice = officeList.find(office => office.id === selectedValue).id;
+        setSelectedOffice(selectedOffice);
+        const officeData = photoSouvenirsList.find((office) => office.id === selectedValue);
+        setSelectedOfficeData(officeData);
+    }, [selectedValue]);
     return (
         <Layout>
             <div className="col-md-12">
                 <h3>Фотосувениры</h3>
             </div>
+            {selectedOfficeData &&
+                selectedOfficeData.services.map((service) => (
+                    <CategoryInfo key={service.name} officeId={selectedOffice} categoryName={service.name} responseData={photoSouvenirsList} />
+                ))
+            }
         </Layout>
     );
 }
