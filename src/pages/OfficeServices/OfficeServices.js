b2dcd498cd8ad53251fad9f44463ec50fccd85a5
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-27 08:00:12 +0300

    add useSelectedValue, CategoryInfo component

--- src/pages/OfficeServices/OfficeServices.js
+++ src/pages/OfficeServices/OfficeServices.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Layout from "../../components/Layout/Layout";
+import {useSelectedValue} from "../../context/SelectedValueContext";
+import officeList from "../../data/officeList";
+import CategoryInfo from "../../components/CategoryInfo/CategoryInfo";
+import officeServices from "../../data/officeServicesList";
 
 function OfficeServices() {
+    const { selectedValue } = useSelectedValue();
+    const [selectedOffice, setSelectedOffice] = useState(1);
+    const [selectedOfficeData, setSelectedOfficeData] = useState(null);
+
+    useEffect(() => {
+        const selectedOffice = officeList.find(office => office.id === selectedValue).id;
+        setSelectedOffice(selectedOffice);
+        const officeData = officeServices.find((office) => office.id === selectedValue);
+        setSelectedOfficeData(officeData);
+    }, [selectedValue]);
     return (
         <Layout>
             <div className="col-md-12">
                 <h3>Офисные услуги</h3>
             </div>
+            {selectedOfficeData &&
+                selectedOfficeData.services.map((service) => (
+                    <CategoryInfo key={service.name} officeId={selectedOffice} categoryName={service.name} responseData={officeServices} />
+                ))
+            }
         </Layout>
     );
 }
