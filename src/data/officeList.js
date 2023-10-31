d581c892b0cb9f1c7a5852b01aa904f612a1ab31
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-31 13:33:19 +0300

    add fields with schedule, mail, vk, organizationName, inn, ogrnip for data officeList

--- src/data/officeList.js
+++ src/data/officeList.js
@@ -4,35 +4,84 @@ const officeList = [
         city: 'Ярославль',
         address: 'пос. Красный бор, с1',
         department: 'ТЦ «ГЛОБУС»',
-        phone: '8(980)709-08-40'
+        phone: '8(980)709-08-40',
+        schedule: 'ежедневно 09:00-22:00',
+        email: '89807090840@mail.ru',
+        vk: [
+            {
+                name: 'Фотосалон А4 | Ярославль | Глобус',
+                link: 'https://vk.com/fotosalonyaroslavl'
+            }
+        ],
+        organizationName: 'ИП Долотов Никита Артурович',
+        inn: '501802492360',
+        ogrnip: '314501813600044'
     },
     {
         id: 2,
         city: 'Ярославль',
         address: 'Ленинградский пр-кт, 54А',
         department: 'ТЦ «ЛЕНТА»',
-        phone: '8(995)538-08-40'
+        phone: '8(995)538-08-40',
+        schedule: 'ежедневно 09:00-22:00',
+        email: '89955380840@mail.ru',
+        vk: [
+            {
+                name: 'Фотосалон А4 | Ярославль | ЛЕНТА',
+                link: 'https://vk.com/fotosalonyar'
+            }
+        ],
+        organizationName: 'ИП Долотов Никита Артурович',
+        inn: 'ИНН 501802492360',
+        ogrnip: 'ОГРНИП 314501813600044'
     },
     {
         id: 3,
         city: 'Королев',
         address: 'ул. Коммунальная, 1',
         department: 'ТЦ «ГЛОБУС»',
-        phone: '8(916)959-08-40'
+        phone: '8(916)959-08-40',
+        schedule: 'ежедневно 09:00-22:00',
+        email: '89169590840@mail.ru',
+        vk: [
+            {
+                name: 'Фотосалон А4 | Королев | Глобус',
+                link: 'https://vk.com/fotosalonkorolev'
+            }
+        ],
+        organizationName: 'ИП Долотов Никита Артурович',
+        inn: '501802492360',
+        ogrnip: '314501813600044'
     },
     {
         id: 4,
         city: 'Королев',
         address: 'ул. Исаева, 1Б',
         department: 'ТЦ «ЗОДИАК»',
-        phone: '8(916)809-83-02'
+        phone: '8(916)809-83-02',
+        schedule: 'ежедневно 09:00-22:00',
+        email: '89168098302@mail.ru',
+        organizationName: 'ИП Долотов Никита Артурович',
+        inn: 'ИНН 501802492360',
+        ogrnip: 'ОГРНИП 314501813600044'
     },
     {
         id: 5,
         city: 'Москва',
         address: 'ул. Дубравная, 51с2',
         department: 'ТЦ «ГЛОБУС»',
-        phone: '8(985)444-08-40'
+        phone: '8(985)444-08-40',
+        schedule: 'ежедневно 09:00-22:00',
+        email: '89854440840@mail.ru',
+        vk: [
+            {
+                name: 'Фотосалон А4 | Митино | Глобус',
+                link: 'https://vk.com/fotosalonmitino'
+            }
+        ],
+        organizationName: 'ИП Долотов Никита Артурович',
+        inn: 'ИНН 501802492360',
+        ogrnip: 'ОГРНИП 314501813600044'
     }
 ];
 export default officeList;
