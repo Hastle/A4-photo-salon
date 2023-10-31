dfb6215b64e5ac5b8cd21e2d351bccf9431b4ff9
Alexander Egerev <alexandrs.me.com@gmail.com>
2023-10-31 13:20:30 +0300

    create data otherList

---
+++ src/data/otherList.js
@@ -0,0 +1,183 @@
+import Polaroid_1 from "../../public/images/other/polaroid-1.png";
+import Polaroid_2 from "../../public/images/other/polaroid-2.png";
+import Polaroid_3 from "../../public/images/other/polaroid-3.png";
+import Polaroid_4 from "../../public/images/other/polaroid-4.png";
+import Polaroid_5 from "../../public/images/other/polaroid-5.png";
+
+const otherList = [
+    {
+        id: 1,
+        services: [
+            {
+                name: 'Оцифровка',
+                products: [
+                    {
+                        title: 'Видеокассеты',
+                        options: [
+                            {
+                                subtitle: '1 час',
+                                price: '300р'
+                            },
+                        ]
+                    },
+                    {
+                        title: 'Плёнка и слайдеры',
+                        options: [
+                            {
+                                subtitle: '1 кадр',
+                                price: '100р'
+                            },
+                            {
+                                subtitle: '5 кадров',
+                                price: '50р'
+                            },
+                            {
+                                subtitle: '10 кадров',
+                                price: '30р'
+                            },
+                        ]
+                    },
+                ],
+                sideImages: [Polaroid_1],
+            },
+            {
+                name: 'Визитки',
+                products: [
+                    {
+                        title: 'Односторонние (4+0)',
+                        options: [
+                            {
+                                subtitle: '100 шт',
+                                price: '600р'
+                            },
+                            {
+                                subtitle: '200 шт',
+                                price: '1100р'
+                            },
+                            {
+                                subtitle: '300 шт',
+                                price: '1650р'
+                            },
+                        ]
+                    },
+                    {
+                        title: 'Двусторонние (4+4)',
+                        options: [
+                            {
+                                subtitle: '100 шт',
+                                price: '800р'
+                            },
+                            {
+                                subtitle: '200 шт',
+                                price: '1500р'
+                            },
+                            {
+                                subtitle: '300 шт',
+                                price: '1950р'
+                            },
+                        ]
+                    },
+                ],
+                sideImages: [Polaroid_2],
+            },
+            {
+                name: 'Фотокерамика',
+                products: [
+                    {
+                        title: 'Металл',
+                        options: [
+                            {
+                                subtitle: '13х18 см',
+                                price: '2170р'
+                            },
+                            {
+                                subtitle: '18х24 см',
+                                price: '2490р'
+                            },
+                            {
+                                subtitle: '17х22 см',
+                                price: '3210р'
+                            },
+                            {
+                                subtitle: 'Без фото',
+                                price: '1120р'
+                            },
+                        ]
+                    },
+                    {
+                        title: 'Фарфор',
+                        options: [
+                            {
+                                subtitle: '9х13 см',
+                                price: '2170р'
+                            },
+                            {
+                                subtitle: '13х18 см',
+                                price: '2790р'
+                            },
+                            {
+                                subtitle: '18х24 см',
+                                price: '3210р'
+                            },
+                        ]
+                    },
+                    {
+                        title: 'Гранит',
+                        options: [
+                            {
+                                subtitle: '13х18 см',
+                                price: '2460р'
+                            },
+                            {
+                                subtitle: '18х24 см',
+                                price: '2930р'
+                            },
+                            {
+                                subtitle: '20х30 см',
+                                price: '4340р'
+                            },
+                            {
+                                subtitle: '24х30 см',
+                                price: '4640р'
+                            },
+                        ]
+                    },
+                ],
+                sideImages: [Polaroid_3],
+            },
+            {
+                name: 'Домовые таблички',
+                products: [
+                    {
+                        description: 'Актуальные цены и образцы изделий Вы можете уточнить у сотрудников в фотосалоне.'
+                    },
+                ],
+                sideImages: [Polaroid_4],
+            },
+            {
+                name: 'Багетные работы',
+                products: [
+                    {
+                        description: 'Оформление работы в багет подойдет Вам, если работа имеет необычные размеры.'
+                    },
+                    {
+                        description: 'Актуальные цены и образцы изделий Вы можете уточнить у сотрудников в фотосалоне. Обращаем Ваше внимание, что стоимость рассчитывается индивидуально.'
+                    },
+                ],
+            },
+            {
+                name: 'Товары',
+                products: [
+                    {
+                        description: 'У нас Вы можете найти рамки и альбомы для фотографий на любой вкус!'
+                    },
+                ],
+                sideImages: [Polaroid_5],
+            },
+            // Другие товары и услуги
+        ],
+    },
+    // Другие объекты для других id могут быть добавлены здесь, если необходимо.
+];
+
+export default otherList;
