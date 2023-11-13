ec9bd21c01193c5b3a6cd3f3558ef0acde335d63
Hastle <alexandrs.me.com@gmail.com>
2023-11-13 03:21:03 +0300

    use Form component

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -1,18 +1,18 @@
 import React from "react";
 import styles from "../Main/styles.module.sass";
+import Form from "../../components/Form/Form";
+
 
 function Order() {
     return (
         <>
-            <div className={`col-md-5 ${styles.main_features}`}>
+            <div className={`col-md-6 ${styles.main_features}`}>
                 <h1>Оформите заказ на фотопечать прямо сейчас</h1>
                 <p>Внимательно проверяйте информацию</p>
                 <p>Когда заказ будет готов, сотрудники фотосалона свяжутся с вами </p>
             </div>
-            <div className="col">
-                <div>
-
-                </div>
+            <div className="col-md-6">
+                <Form />
             </div>
         </>
     );
