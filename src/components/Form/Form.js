3b2251c7144e59d5b3cc57dbc1a42b3f712cb731
Hastle <alexandrs.me.com@gmail.com>
2023-11-15 16:36:46 +0300

    use onChange handlers

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,24 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../../components/Inputs/Input/Input";
 import Select from "../../components/Inputs/Select/Select";
-import FileUpload from "../../components/Inputs/FileUpload/FileUpload"
+import FileUpload from "../../components/Inputs/FileUpload/FileUpload";
 import styles from "./styles.module.sass";
+
 function Form() {
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [selectedSalon, setSelectedSalon] = useState("");
+    const [photoSize, setPhotoSize] = useState("");
+    const [uploadedFiles, setUploadedFiles] = useState([]);
+
+    const handlePhoneNumberChange = (value) => {
+        setPhoneNumber(value);
+    };
+
+    const handleSalonChange = (value) => {
+        setSelectedSalon(value);
+    };
+
+    const handlePhotoSizeChange = (value) => {
+        setPhotoSize(value);
+    };
+
+    const handleFileUpload = (files) => {
+        setUploadedFiles(files);
+    };
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("Номер телефона:", phoneNumber);
+        console.log("Выбранный салон:", selectedSalon);
+        console.log("Размер фотографий:", photoSize);
+        console.log("Загруженные файлы:", uploadedFiles);
+    };
+
     return (
-        <form>
+        <form onSubmit={handleFormSubmit}>
             <div className={styles.form_group}>
-                <Input label={'Ваш номер телефона'} type={'phone'} placeholder={'Номер телефона'} />
+                <Input
+                    label={"Ваш номер телефона"}
+                    type={"phone"}
+                    placeholder={"Номер телефона"}
+                    onChange={handlePhoneNumberChange}
+                />
             </div>
             <div className={styles.form_group}>
-                <Select label={'Выберите фотосалон'} />
+                <Select label={"Выберите фотосалон"} onChange={handleSalonChange} />
             </div>
             <div className={styles.form_group}>
-                <Input label={'Выберите размер фотографий'} type={'text'} placeholder={'Размер'} />
+                <Input
+                    label={"Выберите размер фотографий"}
+                    type={"text"}
+                    placeholder={"Размер"}
+                    onChange={handlePhotoSizeChange}
+                />
             </div>
             <div className={styles.form_group}>
-                <FileUpload label={'Загрузите фотографии'}/>
+                <FileUpload label={"Загрузите фотографии"} onFileUpload={handleFileUpload} />
+            </div>
+            <div className={styles.form_group}>
+                <button type="submit">Отправить</button>
             </div>
         </form>
-    )
+    );
 }
+
 export default Form;
