0e8fb9c5b598d1d42c5b019ea7c7fc853ab078d8
Hastle <alexandrs.me.com@gmail.com>
2023-12-07 02:04:45 +0300

    refactor Modal component structure and styles

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,21 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CgClose } from "react-icons/cg";
 import styles from "./styles.module.sass";
 
-const Modal = ({ isOpen, onClose, children }) => {
-    if (!isOpen) return null;
-
+const Modal = ({ open, onClose, title, children }) => {
+    const modalActiveStyles = `${styles.modal_overlay} ${styles.modal_overlay_active}`
+    if (!open)
+        return null
     return ReactDOM.createPortal(
-        <div className={styles["modal-overlay"]}>
-            <div className={styles.modal}>
-                <button onClick={onClose} className={styles["close-button"]}>
-                    Закрыть
-                </button>
-                {children}
+        <div className={open ? modalActiveStyles : styles.modal_overlay} onClick={onClose}>
+            <div className={styles.modal} onClick={e => e.stopPropagation()}>
+                <div className={styles.modal_header}>
+                    <h5>{title}</h5>
+                    <button className={styles.btn_close} onClick={onClose}><CgClose style={{strokeWidth: 1}}/></button>
+                </div>
+                <div className={styles.modal_body}>
+                    {children}
+                </div>
+
             </div>
         </div>,
         document.getElementById('modal-root')
-    );
+    )
 };
 
 export default Modal;
