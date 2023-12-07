825a49ba9e825c7585fca5dff8d4714334d67881
Hastle <alexandrs.me.com@gmail.com>
2023-12-08 01:22:28 +0300

    fix Header component issues: updated localStorage value check and improved Modal component logic

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,10 +15,11 @@ function Header() {
     const { selectedValue, setSelectedValue } = useSelectedValue();
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [tempSelectedValue, setTempSelectedValue] = useState(defaultValue);
 
     useEffect(() => {
         const savedValue = localStorage.getItem("selectedOfficeId");
-        if (savedValue) {
+        if (savedValue === null) {
             setSelectedValue(parseInt(JSON.parse(savedValue), 10));
         } else {
             setIsModalOpen(true);
@@ -42,54 +43,85 @@ function Header() {
         setSelectedValue(parseInt(newValue, 10));
         closeMenu();
     };
+
+    const handleCheckboxConfirm = () => {
+        setSelectedValue(tempSelectedValue);
+        localStorage.setItem("selectedOfficeId", JSON.stringify(selectedValue));
+        setIsModalOpen(false);
+    };
+
     return (
         <>
-        <header className={styles.header}>
-            <div className={styles.main_menu}>
-                <div className={styles.navigation_layer}>
-                    <Logotype onClick={closeMenu}/>
-                    <Burger onClick={toggleMenu} isOpen={isMenuOpen} Component="button" type="button" />
-                </div>
-                <nav className={isMenuOpen ? styles.top_navigation_active : styles.top_navigation}>
-                    <NavLink className={styles.menu_item} to="/photo-services" onClick={closeMenu}>
-                        Фотоуслуги
-                    </NavLink>
-                    <NavLink className={styles.menu_item} to="/photo-souvenirs" onClick={closeMenu}>
-                        Фотосувениры
-                    </NavLink>
-                    <NavLink className={styles.menu_item} to="/office-services" onClick={closeMenu}>
-                        Офисные услуги
-                    </NavLink>
-                    <NavLink className={styles.menu_item} to="/other" onClick={closeMenu}>
-                        Другое
-                    </NavLink>
-                    <NavLink className={styles.menu_item} to="/order" onClick={closeMenu}>
-                        Заказ
-                    </NavLink>
-                    <NavLink className={styles.menu_item} to="/contacts" onClick={closeMenu}>
-                        Контакты
-                    </NavLink>
-                    <div className={styles.menu_item}>
-                        <MdLocationOn className={styles.icon} />
-                        <select
-                            name="address-current"
-                            value={selectedValue ? selectedValue.toString() : defaultValue.toString()}
-                            onChange={handleSelectChange}
-                        >
-                            {officeList.map((office) => (
-                                <option key={office.id} value={office.id}>
-                                    {`${office.city}, ${office.department}`}
-                                </option>
-                            ))}
-                        </select>
+            <header className={styles.header}>
+                <header className={styles.header}>
+                    <div className={styles.main_menu}>
+                        <div className={styles.navigation_layer}>
+                            <Logotype onClick={closeMenu} />
+                            <Burger onClick={toggleMenu} isOpen={isMenuOpen} Component="button" type="button" />
+                        </div>
+                        <nav className={isMenuOpen ? styles.top_navigation_active : styles.top_navigation}>
+                            <NavLink className={styles.menu_item} to="/photo-services" onClick={closeMenu}>
+                                Фотоуслуги
+                            </NavLink>
+                            <NavLink className={styles.menu_item} to="/photo-souvenirs" onClick={closeMenu}>
+                                Фотосувениры
+                            </NavLink>
+                            <NavLink className={styles.menu_item} to="/office-services" onClick={closeMenu}>
+                                Офисные услуги
+                            </NavLink>
+                            <NavLink className={styles.menu_item} to="/other" onClick={closeMenu}>
+                                Другое
+                            </NavLink>
+                            <NavLink className={styles.menu_item} to="/order" onClick={closeMenu}>
+                                Заказ
+                            </NavLink>
+                            <NavLink className={styles.menu_item} to="/contacts" onClick={closeMenu}>
+                                Контакты
+                            </NavLink>
+                            <div className={styles.menu_item}>
+                                <MdLocationOn className={styles.icon} />
+                                <select
+                                    name="address-current"
+                                    value={selectedValue ? selectedValue.toString() : defaultValue.toString()}
+                                    onChange={handleSelectChange}
+                                >
+                                    {officeList.map((office) => (
+                                        <option key={office.id} value={office.id}>
+                                            {`${office.city}, ${office.department}`}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                        </nav>
                     </div>
-                </nav>
-            </div>
-        </header>
-        <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)} title={"Выберите магазин"}>
-            <p className="modal-content">В разных магазинах цены на товары и услуги могут отличатся. Пожалуйста, выберите удобный для вас магазин.</p>
-        </Modal>
-    </>
+                </header>
+                {isModalOpen && (
+                    <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)} title={"Выберите фотосалон"}>
+                        <p className="modal-content">
+                            В разных магазинах цены на товары и услуги могут отличаться. Пожалуйста, выберите удобный для вас
+                            адрес.
+                        </p>
+                        {officeList.map((office) => (
+                            <div className="address-item" key={office.id}>
+                                <input
+                                    type="checkbox"
+                                    value={office.id}
+                                    checked={tempSelectedValue === office.id}
+                                    onChange={() => setTempSelectedValue(office.id)}
+                                />
+                                <div className="address-info">
+                                    <p>
+                                        {`${office.department}, ${office.city}, ${office.address}`}
+                                    </p>
+                                    <span>{office.schedule}</span>
+                                </div>
+                            </div>
+                        ))}
+                        <button onClick={handleCheckboxConfirm}>Выбрать</button>
+                    </Modal>
+                )}
+            </header>
+        </>
     );
 }
 
